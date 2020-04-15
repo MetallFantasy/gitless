5b6a17c413535cbc88e168f5122ae93a6f9ba929
MetallFantasy <metallfantasy@gmail.com>
2020-04-15 16:03:02 +0300

    MyFirstCommit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PinkBox {
+    class Program {
+        static void Main(string[] args)
+        {
+            string name = "Metall";
+            Console.WriteLine("Hello ", name);
+            Console.ReadKey();
+        }
+    }
+}
