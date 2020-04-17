4f16158a3dac794b22ff465a78d6896b394d2ff4
MetallFantasy <metallfantasy@gmail.com>
2020-04-17 23:11:19 +0300

    Modified class Size

--- Size.cs
+++ Size.cs
@@ -4,5 +4,27 @@ using System.Text;
 
 namespace PinkBox {
     class Size {
+        private int height; // Высота
+        private int width; // Ширина
+        private int length; // Длина
+
+        public Size(int height, int width, int length)
+        {
+            if (height > 0 && height < 100)
+            {
+                this.height = height;
+            }
+            //else
+            if (width > 0 && width < 100)
+            {
+                this.width = width;
+            }
+            //else
+            if (length > 0 && length < 100)
+            {
+                this.length = length;
+            }
+            //else
+        }
     }
 }
